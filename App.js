d4921e1393d51ada00801132f63197b685dc4fa5
samuelmuittari <muittaris@gmail.com>
2023-01-17 18:43:37 +0200

    done

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [input1, setInput1] = React.useState(0)
+  const [input2, setInput2] = React.useState(0)
+  const [result, setResult] = React.useState(0)
+  const add = () => {
+     let addedInputs = ((+input1) + (+input2))
+     setResult(addedInputs)
+  }
+  const subtract = () => {
+     let subtractedInputs = ((+input1) - (+input2))
+     setResult(subtractedInputs)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput 
+        style={{ width: 150, borderColor: 'gray', borderWidth: 1 }}
+        value={input1}
+        onChangeText={input1 => setInput1(input1)}
+        keyboardType='numeric'
+      />
+      <TextInput 
+      style={{width: 150, borderColor: 'gray', borderWidth: 1}}
+      value={input2}
+      onChangeText={input2 => setInput2(input2)}
+      keyboardType='numeric'
+      />
+      <View style={styles.button}>
+        <Button id="add" title="+" onPress={add} />
+        <Button id="subtract" title="-" onPress={subtract}/>
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -17,4 +47,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    flexDirection: 'row',
+  }
+ 
+
 });
